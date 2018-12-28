9203267cba99c725e03b0ec1311e53e79585ec4e
chenyangbiliang <chenyangbiliang@leedarson.com>
2018-12-28 20:30:01 +0800

    111

--- app/Greeter.js
+++ app/Greeter.js
@@ -10,12 +10,14 @@
 //Greeter,js
 import React, {Component} from 'react'
 import config from './config.json';
+import styles from './Greeter.css';//导入
 
 class Greeter extends Component{
   render() {
     return (
-      <div>
-        {config.greetText}
+      // <div >
+      <div className={styles.root}> 
+       <span className="cybl">{config.greetText}</span> 
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+var webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
 module.exports = {
   devtool: 'eval-source-map',
   entry: __dirname + "/app/main.js", //已多次提及的唯一入口文件
   output: {
-    path: __dirname + "/public", //打包后的文件存放的地方
+    path: __dirname + "/build", //打包后的文件存放的地方
     filename: "bundle.js" //打包后输出文件的文件名
   },
   devServer: {
@@ -26,19 +30,27 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          {
+        use: [{
             loader: "style-loader"
           },
           {
             loader: "css-loader",
             options: {
               modules: true, // 指定启用css modules
-              localIdentName: '[name]__[local]--[hash:base64:5]' // 指定css的类名格式
-          }
+              localIdentName: '[name]__[local]--[hash:base64:5]', // 指定css的类名格式
+            }
+          },
+          {
+            loader: "postcss-loader"
           }
         ]
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin('版权所有，翻版必究cybl'),
+    new HtmlWebpackPlugin({
+      template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+  }),
+  ],
 }
